94c93405db4341b1cd4200aa40cb489146afbf08
Alberto <74522184+albim360@users.noreply.github.com>
2023-05-26 17:33:04 +0200

    FEAT category modified

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,30 @@
 <template>
-  <header>
-    <HeaderComponent />
-  </header>
+    <header>
+      <HeaderComponent />
+      <CategoryCardComponent/>
+    </header>
 
-  <footer>
-    <FooterComponent />
-  </footer>
-
- 
+    <footer>
+      <FooterComponent />
+    </footer>
 </template>
 
 <script>
 import HeaderComponent from './components/HeaderComponent.vue';
 import FooterComponent from './components/footer_partials/FooterComponent.vue';
+import CategoryCardComponent from './components/slider_category_partials/CategoryCardComponent.vue';
 
 export default {
-  components: { HeaderComponent, FooterComponent }, 
-
-  
-
+  components: {
+    HeaderComponent,
+    FooterComponent,
+    CategoryCardComponent
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 
+
 </style>
+
