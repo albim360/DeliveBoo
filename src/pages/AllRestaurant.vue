c21bbc3a825876b59e9ea679b6930d3e9eb53722
Alberto <74522184+albim360@users.noreply.github.com>
2023-05-29 19:46:35 +0200

    FIX category show

--- src/pages/AllRestaurant.vue
+++ src/pages/AllRestaurant.vue
@@ -1,34 +1,59 @@
 <template>
     <div>
       <h2>All Restaurants</h2>
-      <div v-for="restaurant in filteredRestaurants" :key="restaurant.id">
-        {{ restaurant.company_name }}
+      <div v-for="restaurant in restaurants" :key="restaurant.id" class="card">
+        <div class="card-content">
+          <div>
+            <strong>{{ restaurant.company_name }}</strong>
+          </div>
+          <div>
+            {{ restaurant.address }}
+          </div>
+        </div>
       </div>
     </div>
   </template>
   
+  <style scoped>
+  .card {
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    margin-bottom: 10px;
+    padding: 10px;
+  }
+  
+  .card-content {
+    margin-top: 5px;
+  }
+  </style>
+  
+  
   <script>
+  import axios from 'axios';
+  
   export default {
+    data() {
+      return {
+        restaurants: [] 
+      };
+    },
     props: {
-      restaurants: {
-        type: Array,
-        default: () => [],
-      },
       selectedCategoryId: {
         type: Number,
         default: null,
       },
     },
-    computed: {
-      filteredRestaurants() {
-        if (!this.selectedCategoryId) {
-          return this.restaurants;
-        } else {
-          return this.restaurants.filter((restaurant) => {
-            return restaurant.typologies.some((typology) => typology.id === this.selectedCategoryId);
-          });
+    async mounted() {
+      try {
+        const response = await axios.get(`http://localhost:8000/api/typologies/${this.selectedCategoryId}`);
+        if (response.data.results) {
+          console.log(response);
+          this.restaurants = response.data.results;
+          console.log(this.restaurants);
         }
-      },
+      } catch (error) {
+        console.error(error);
+      }
     },
   };
   </script>
