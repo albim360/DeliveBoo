51da581213ecd077810b02e4aa2dd32b8c36faa8
Francesco Punta <puntafrancesco@gmail.com>
2023-05-31 13:09:04 +0200

    Test for fix the searchbar. Need an API call

--- src/pages/AdvancedSearch.vue
+++ src/pages/AdvancedSearch.vue
@@ -1,108 +1,141 @@
 <template>
     <div class="container card my-4">
-        <h1>Ricerca Avanzata</h1>
-        <div class="row">
-            <div class="col-12">
-                <label for="rest-name">Nome ristorante</label>
-                <input class="name-search" v-model="searchQuery" type="text" name="rest-name">
-            </div>
+      <h1>Ricerca Ristoranti</h1>
+      <div class="search-bar">
+        <input class="search-input" v-model="searchQuery" type="text" placeholder="Cerca per nome ristorante">
+        <button class="search-btn" @click="startSearch">Cerca</button>
+      </div>
+      <div class="row d-flex card-body">
+        <div class="col row col-type d-flex">
+          <label for="cook-type">Cucina</label>
+          <div class="form-check col-2" v-for="typology in sortedTypologies" :key="typology.slug">
+            <input class="form-check-input" type="checkbox" :value="typology.id" id="typology-{{typology.slug}" v-model="selectedTypologies">
+            <label class="form-check-label text-capitalize" for="typology-{{typology.slug}">{{ typology.slug }}</label>
+          </div>
         </div>
-        <div class="row d-flex card-body">
-            <div class="col row col-type d-flex ">
-                <label for="cook-type">Tipo di cucina</label>
-                <div class="form-check col-2" v-for="typology in sortedTypologies" :key="typology.slug">
-                    <input class="form-check-input" type="checkbox" :value="typology.id" id="typology-{{typology.slug}}">
-                    <label class="form-check-label text-capitalize" for="typology-{{typology.slug}}">{{ typology.slug
-                    }}</label>
-                </div>
-            </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <h3 class="result-title">Risultati:</h3>
+          <ul class="result-list">
+            <li v-for="restaurant in filteredRestaurants" :key="restaurant.id" class="result-item">
+              <h4>{{ restaurant.company_name }}</h4>
+              <p>{{ getPreviewText(restaurant.description, searchQuery) }}</p>
+            </li>
+          </ul>
         </div>
-        <div class="row">
-            <div class="col-2" v-for="restaurant in filteredRestaurants" :key="restaurant.id">
-                {{ restaurant.company_name }}
-            </div>
-        </div>
-
-
+      </div>
     </div>
-</template>
-
-<script>
-import axios from 'axios';
-
-export default {
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
     data() {
-        return {
-            typologies: [],
-            restaurants: [],
-            filteredRestaurants: [],
-        }
+      return {
+        typologies: [],
+        searchQuery: '',
+        selectedTypologies: [],
+        filteredRestaurants: [],
+      };
     },
     methods: {
-        fetchTypologies() {
-            axios
-                .get('http://127.0.0.1:8000/api/typologies')
-                .then(res => {
-                    console.log(res.data.results[0].slug); // stampa la risposta dell'API nella console
-                    this.typologies = res.data.results;
-                })
-                .catch(error => {
-                    console.error(error);
-                });
-        },
-
-        fetchRestaurants() {
-            axios.get('http://127.0.0.1:8000/api/restaurants').then(res => {
-                // console.log(res.data.results)
-                this.restaurants = res.data.results;
-                console.log(this.restaurants)
-            }).catch(error => {
-                console.log(error);
-            });
-        },
-
-        // filteredRestaurants() {
-        //     return searchQuery => {
-        //         const lowerCaseQuery = searchQuery.toLowerCase();
-        //         return this.restaurants.filter(restaurants => restaurants.company_name.toLowerCase().includes(lowerCaseQuery));
-        //     };
-        // }
-    },
-
-
-    computed: {
-        sortedTypologies: function () {
-            return this.typologies.sort(function (a, b) {
-                return a.slug.localeCompare(b.slug);
-            });
-        },
-
-        filteredRestaurants() {
-            const lowerCaseQuery = this.searchQuery.toLowerCase();
-            return this.restaurants.filter(restaurant =>
-                restaurant.company_name.toLowerCase().includes(lowerCaseQuery)
-            );
+      fetchTypologies() {
+        axios.get('/api/typologies')
+          .then(response => {
+            this.typologies = response.data.results;
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      },
+      startSearch() {
+        axios.get('/api/advanced-search', {
+          params: {
+            search: this.searchQuery,
+            typologies: this.selectedTypologies,
+          },
+        })
+        .then(response => {
+          this.filteredRestaurants = response.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+      },
+      getPreviewText(text, query) {
+        if (query && text) {
+          const lowerCaseQuery = query.toLowerCase();
+          const words = text.split(' ');
+          const highlightedWords = words.map(word => {
+            if (word.toLowerCase().includes(lowerCaseQuery)) {
+              return `<span class="highlight">${word}</span>`;
+            }
+            return word;
+          });
+          return highlightedWords.join(' ');
         }
+        return text;
+      },
     },
-    // watch: {
-    //     searchQuery() {
-    //         this.filteredRestaurants();
-    //     }
-    // },
+    computed: {
+      sortedTypologies() {
+        return this.typologies.sort((a, b) => a.slug.localeCompare(b.slug));
+      },
+    },
     created() {
-        this.fetchTypologies();
-        this.fetchRestaurants();
-        console.log(this.res)
-    }
-}
-</script>
-
-<style lang="scss" scoped>
-// .col-type, .col-distance, .col-delivery{
-//     display: flex;
-// }
-
-.name-search {
-    width: 100%;
-}
-</style>
+      this.fetchTypologies();
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .search-bar {
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+  
+  .search-input {
+    flex-grow: 1;
+    padding: 8px;
+    border-radius: 4px;
+    border: 1px solid #ccc;
+    font-size: 16px;
+  }
+  
+  .search-btn {
+    margin-left: 10px;
+    padding: 8px 16px;
+    background-color: #ffcc00;
+    color: #ffffff;
+    border: none;
+    border-radius: 4px;
+    font-size: 16px;
+    cursor: pointer;
+  }
+  
+  .result-title {
+    margin-top: 20px;
+    font-size: 24px;
+  }
+  
+  .result-list {
+    list-style: none;
+    padding: 0;
+    margin-top: 10px;
+  }
+  
+  .result-item {
+    margin-bottom: 20px;
+  }
+  
+  .highlight {
+    background-color: #ffcc00;
+    color: #ffffff;
+    padding: 2px 4px;
+  }
+  
+  </style>
+  
