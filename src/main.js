63e75a2f7ba5bec508c0a431af80c57bf55ddaaa
Francesco Punta <puntafrancesco@gmail.com>
2023-06-01 12:08:55 +0200

    Installing FontAwsome

--- src/main.js
+++ src/main.js
@@ -4,5 +4,27 @@ import App from './App.vue'
 import router from './router';
 import 'bootstrap/dist/css/bootstrap.css';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPizzaSlice, faBowl, faSushi, faUtensils, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faInstagram, faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+
+library.add(
+  faPizzaSlice,
+  faBowl,
+  faSushi,
+  faUtensils,
+  faSearch,
+  faInstagram,
+  faFacebook,
+  faTwitter,
+  farStar
+);
+
+
+
+
 
 createApp(App).use(router).mount('#app')
