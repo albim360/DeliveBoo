e6e78730648ebd4b1c5c6e61e1ca6136fcae8b31
Giovannirighini2000 <righinigiovanni0@gmail.com>
2023-06-01 17:32:38 +0200

    clean error

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import CartPage from "./pages/CartPage.vue";
+
 import HomePage from "./pages/HomePage.vue";
 import AllRestaurant from "./pages/AllRestaurant.vue";
 import SingleRestaurant from "./pages/SingleRestaurant.vue";
@@ -62,11 +62,7 @@ const router = createRouter({
       name: 'not-found',
       component: NotFound,
     },
-    {
-      path: "/cart",
-      name: "cart",
-      component: CartPage,
-    },
+
   ],
 });
 
