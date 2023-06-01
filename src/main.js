6f5597eb1d4c68c6c5f382716889e2d4ae1ae4b8
Francesco Punta <puntafrancesco@gmail.com>
2023-06-01 12:59:20 +0200

    Install packages and importing.

--- src/main.js
+++ src/main.js
@@ -4,27 +4,14 @@ import App from './App.vue'
 import router from './router';
 import 'bootstrap/dist/css/bootstrap.css';
 
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faPizzaSlice, faBowl, faSushi, faUtensils, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faUtensils, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { faInstagram, faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import '@fortawesome/fontawesome-svg-core/styles.css';
-
-library.add(
-  faPizzaSlice,
-  faBowl,
-  faSushi,
-  faUtensils,
-  faSearch,
-  faInstagram,
-  faFacebook,
-  faTwitter,
-  farStar
-);
 
 
+library.add(faUtensils, faSearch, faInstagram, faFacebook, faTwitter, farStar);
 
 
-
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
